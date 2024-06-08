22f49413cd2b42bbe4634b537a916b9a4e7a7ff5
Lucia Velasco <luciakvelasco@gmail.com>
2024-06-08 16:40:48 +0100

    Add Tied state and a bit of styling
    Also adds a fix to the diagonal win detection that I forgot to add to the last commit.
    
    In a very Atomic Commits Style codebase I would add it as a separate commit (`fix: check for a reverse diagonal winner`).

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,35 +6,39 @@ export const Main = () => {
   const [board, setBoard] = useState<Board>(newBoard())
   const [currentPlayer, setCurrentPlayer] = useState<XorO>('X')
   const [winner, setWinner] = useState<XorO | undefined>(undefined)
+  const [turnsPlayed, setTurnsPlayed] = useState<number>(0)
 
   useEffect(() => {
     const checkWinnerInRows = (board: Board): void => board.forEach(row =>
       (row[ 0 ] === 'X' || row[ 0 ] === 'O') && row.every(value => value === row[ 0 ]) && setWinner(row[ 0 ]))
 
-    // check rows
     checkWinnerInRows(board)
     const columnsToRows = [...new Array(board.length)].map((v, column) => board.map(row => row[ column ]))
     checkWinnerInRows(columnsToRows)
-    const diagonalToRows = [board.map((row, i) => board[ i ][ i ])]
+    const diagonalToRows = [
+      board.map((row, i) => board[ i ][ i ]),
+      [...board].reverse().map((row, i, reversedBoard) => reversedBoard[ i ][ i ])
+    ]
     checkWinnerInRows(diagonalToRows)
 
   }, [board])
 
   return <div className="flex flex-col mt-10 items-center gap-10">
-    <h1 className="font-bold text-2xl">Tic Tac Toe</h1>
+    <h1 className="font-bold text-5xl">Tic Tac Toe</h1>
     <div className="flex flex-col gap-1">
       {board.map((row, rowIndex) =>
         <div className="flex gap-1" key={`tictactoe-${rowIndex}`}>
           {row.map((cell, columnIndex) =>
             <div
               key={`tictactoe-${rowIndex}${columnIndex}`}
-              className="border-2 border-gray-900 w-10 h-10 cursor-pointer items-center justify-center text-2xl font-bold flex"
+              className="border-2 border-gray-900 w-20 h-20 cursor-pointer items-center justify-center text-7xl font-bold flex"
               onClick={() => {
-                if (cell === undefined) {
+                if (!winner && cell === undefined) {
                   const newBoard: Board = [...board]
                   newBoard[ rowIndex ][ columnIndex ] = currentPlayer
                   setBoard(newBoard)
                   setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X')
+                  setTurnsPlayed(turnsPlayed + 1)
                 }
               }}
             >
@@ -44,14 +48,23 @@ export const Main = () => {
         </div>
       )}
     </div>
-    {winner && <p className="font-bold text-xl">GAME OVER. {winner} WON!</p>}
+
     <button
+      className="rounded-full bg-green-300 px-8 py-4 text-lg font-medium"
       onClick={() => {
         setBoard(newBoard())
         setWinner(undefined)
         setCurrentPlayer('X')
-      }}>
-      Reset Game
+        setTurnsPlayed(0)
+      }}
+    >
+      reset game
     </button>
+
+    {winner
+      ? <p className="font-bold text-3xl">Game over, {winner} won!</p>
+      : turnsPlayed === (board.length ** 2)
+        ? <p className="font-bold text-3xl">Game over: no winner.</p>
+        : null}
   </div>
 }
